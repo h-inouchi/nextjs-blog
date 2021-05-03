ddcdac4cdbe43ce0595dc110e0f4daf9e819405b
h-inouchi <hibird2720804@gmail.com>
2021-05-03 16:29:53 +0900

    サイトのタイトル変更

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'ミーアキャットブログ'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'ミーアキャットブログ'
 
 export default function Layout({ children, home }) {
   return (
