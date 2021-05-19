00a0e29b88d6e84414c3ba4a71dc6b757e847a09
h-inouchi <hibird2720804@gmail.com>
2021-05-19 17:47:46 +0900

    Movies Page がわだけ

---
+++ src/components/movies/MovieList.tsx
@@ -0,0 +1,31 @@
+import Grid from '@material-ui/core/Grid';
+import PostListItem from 'src/components/posts/PostListItem';
+import { useQuery } from '@apollo/client';
+import { RANDOM_MEERKAT_QUERY, RandomMeerkatData } from '../../../lib/random-meerkat';
+import { NextPage } from 'next';
+
+interface MovieListProps {}
+
+const MovieList: NextPage<MovieListProps> = () => {
+  const { loading, error, data } = useQuery<RandomMeerkatData>(RANDOM_MEERKAT_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
+
+  const { randomMeerkat } = data;
+  if (!randomMeerkat) return null;
+
+  return (
+    <Grid container alignItems="center" justify="center">
+      <Grid key={randomMeerkat.id} item xs={12} sm={10} md={4} lg={4}>
+        <a href={`/meerkats/${randomMeerkat.id}`}>
+          <PostListItem
+            {...randomMeerkat}
+          />
+        </a>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MovieList;
