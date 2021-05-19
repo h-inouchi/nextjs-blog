5378f8259ac5ff6d7cc85c41561f604a26e9580e
h-inouchi <hibird2720804@gmail.com>
2021-05-20 08:35:36 +0900

    add movie url sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -16,8 +16,6 @@ export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => 
 };
 
 async function generateSitemapXml(): Promise<string> {
-  let xml = `<?xml version="1.0" encoding="UTF-8"?>`;
-  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
   const meerkats = await client.query({
     query: gql`
       query Meerkats{
@@ -34,11 +32,55 @@ async function generateSitemapXml(): Promise<string> {
       lastmod: meerkat.photoDate
     }
   })
+  const movies = await client.query({
+    query: gql`
+      query Movies{
+        allMovies {
+          id
+          movieDate
+        }
+      }
+    `
+  })
+  const movieIds = movies.data["allMovies"].map(movie => {
+    return {
+      id: movie.id,
+      lastmod: movie.movieDate
+    }
+  })
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>`;
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+  xml += `
+    <url>
+      <loc>https://nextjs-blog-sigma-two-26.vercel.app</loc>
+      <lastmod>2021-05-20T21:39:19+00:00</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `
+  xml += `
+    <url>
+      <loc>https://nextjs-blog-sigma-two-26.vercel.app/movies</loc>
+      <lastmod>2021-05-20T21:39:19+00:00</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  `
   meerkatIds.forEach((meerkatId) =>{
     xml += `
       <url>
         <loc>https://nextjs-blog-sigma-two-26.vercel.app/meerkats/${meerkatId.id}</loc>
         <lastmod>${meerkatId.lastmod}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>
+    `
+  })
+  movieIds.forEach((movieId) =>{
+    xml += `
+      <url>
+        <loc>https://nextjs-blog-sigma-two-26.vercel.app/movies/${movieId.id}</loc>
+        <lastmod>${movieId.lastmod}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
       </url>
